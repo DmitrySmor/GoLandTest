2b3e62ea1b2f7828149e066a079f826662759456
Admin <dmitrsmor@yandex.ru>
2020-11-19 06:49:08 +0300

    Новые поправки

--- 001_hello_go/main.go
+++ 001_hello_go/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+/*sdafkjaskjfdhaslkjdfhjklsad*/
+
 func main() {
 	fmt.Println("Hello Go", "====== I am best man")
 }
--- 002_packgage/nested/nested.go
+++ 002_packgage/nested/nested.go
@@ -3,6 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Start program...")
-	fmt.Println("Start program...")
+	fmt.Println("Start program.1111..")
 }
